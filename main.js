38bac848236e079e4fe32182b60ebb704d6c16e3
Capabay <capabay@hotmail.com>
2021-02-14 14:03:54 +0100

    render dom

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ async function getData(){
         console.log(error)
     }
 }
+
+//Render to the DOM
+async function renderUsers() {
+    let users = await getUsers();
+    let html = '';
+    users.forEach(user => {
+        let htmlSegment = `<div class="user">
+                            <img src="${user.profileURL}" >
+                            <h2>${user.firstName} ${user.lastName}</h2>
+                            <div class="email"><a href="email:${user.email}">${user.email}</a></div>
+                        </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+
+renderUsers();
