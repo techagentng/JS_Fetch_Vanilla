5c67eace197cd7b72651ae8bbf45974cf7d79168
Capabay <capabay@hotmail.com>
2021-02-14 13:47:29 +0100

    more commit

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ async function getData(){
     let url = 'fetch_data.json';
 
     try{
-        let resonse = await fetch()
-    } catch{
-
+        let resonse = await fetch(url)
+        return await resonse.json();
+    } catch(error){
+        console.log(error)
     }
 }
